0a3d8a5f04c7f8700c33924b5a9ab34ba361d1e5
Sabrina <sabreyes01@gmail.com>
2026-04-18 21:42:40 -0400

    start incorporating modal + api

--- src/api/card.ts
+++ src/api/card.ts
@@ -1,6 +1,5 @@
 import { supabaseFetch } from "./client"
 import { useFetchData } from "../hooks/useFetchData";
-import { useMutateData } from "../hooks/useMutateData";
 
 interface CreateCardInterface {
     status: string;
@@ -17,13 +16,6 @@ export type CardData = {
     column_id: string;
 }
 
-const cardInit = {
-    title: '',
-    description: '',
-    status: '',
-    column_id: ''
-}
-
 const PATH = 'card';
 
 export const createCard = (body: CreateCardInterface) => supabaseFetch(`${PATH}`, { method: "POST", body: JSON.stringify(body) });
@@ -31,9 +23,4 @@ export const createCard = (body: CreateCardInterface) => supabaseFetch(`${PATH}`
 export const useGetCardsByBoardId = (boardId: string) => {
     const { data, loading, error } = useFetchData<CardData[]>(`${PATH}?board_id=eq.${boardId}&limit=4`, !!boardId);
     return { cards: data ?? [], loading, error};
-}
-
-export const useCreateCard = (body: CreateCardInterface) => {
-    const { data, loading, error } = useMutateData<CardData[]>(`${PATH}`, body );
-    return { card: data ?? cardInit, loading, error};
 }
